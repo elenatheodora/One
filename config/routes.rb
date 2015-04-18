ecd616be3ad7d76384d787d143fe860b44234081
Elena Georgieva <elena.theodora@gmail.com>
2015-04-18 12:38:08 -0700

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  resources :ideas
+
+  get 'welcome/aboard'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root :to => redirect('/ideas')
+get "pages/info"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
